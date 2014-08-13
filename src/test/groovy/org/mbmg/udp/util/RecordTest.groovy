0017a5d2a1c68cbd521270641c1e793a11c6f2c1
Ron Pomeroy <rpomeroy@me.com>
2014-08-12 23:22:04 -0700

    Record.toGraphite() method to represent channel data as a List<String> in Graphite format

---
+++ src/test/groovy/org/mbmg/udp/util/RecordTest.groovy
@@ -0,0 +1,19 @@
+package org.mbmg.udp.util
+
+/**
+ * Created by rpomeroy on 8/12/14.
+ */
+class RecordTest extends GroovyTestCase {
+    void setUp() {
+        super.setUp()
+
+    }
+
+    void tearDown() {
+
+    }
+
+    void testToGraphite() {
+
+    }
+}
